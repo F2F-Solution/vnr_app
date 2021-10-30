a676fc51a58e37b6559d6acc930918b606b914d4
balasundarambui <56162438+balasundarambui@users.noreply.github.com>
2021-10-30 11:59:03 +0530

    Login page design

--- themes/vnrpolice/template_signin.php
+++ themes/vnrpolice/template_signin.php
@@ -10,9 +10,9 @@ $theme_path = $this->config->item('theme_locations') . 'vnrpolice';
 
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 
-			<title>Login</title>
+			<title>VNR Police</title>
 
-			<link rel="shortcut icon" href="<?php echo $theme_path ?>/assets/media/logos/favicon.ico" />
+			<link rel="shortcut icon" href="<?php echo $theme_path ?>/assets/media/logos/logo.png" />
 
 			<!--begin::Fonts-->
 			<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
@@ -27,27 +27,27 @@ $theme_path = $this->config->item('theme_locations') . 'vnrpolice';
 			<!--begin::Authentication - Sign-in -->
 			<div class="d-flex flex-column flex-lg-row flex-column-fluid">
 				<!--begin::Aside-->
-				<div class="d-flex flex-column flex-lg-row-auto w-xl-600px positon-xl-relative" style="background-color: #F2C98A">
+				<div class="d-flex flex-column flex-lg-row-auto w-xl-600px positon-xl-relative" style="background-color: #ffe5e6">
 					<!--begin::Wrapper-->
 					<div class="d-flex flex-column position-xl-fixed top-0 bottom-0 w-xl-600px scroll-y">
 						<!--begin::Content-->
 						<div class="d-flex flex-row-fluid flex-column text-center p-10 pt-lg-20">
 							<!--begin::Logo-->
 							<a href="#" class="py-9 mb-5">
-								<img alt="Logo" src="<?php echo $theme_path; ?>/assets/media/logos/logo-2.svg" class="h-60px" />
+								<img alt="Logo" src="<?php echo $theme_path; ?>/assets/media/logos/logo.png" class="h-150px" />
 							</a>
 							<!--end::Logo-->
 							<!--begin::Title-->
-							<h1 class="fw-bolder fs-2qx pb-5 pb-md-10" style="color: #986923;">Welcome to VNR Police</h1>
+							<h1 class="fw-bolder fs-2qx pb-5 pb-md-10" style="color: #c51016;">Welcome to VNR Police</h1>
 							<!--end::Title-->
 							<!--begin::Description-->
-							<p class="fw-bold fs-2" style="color: #986923;">We Rise by lifting
-							<br />Others</p>
+							<!-- <p class="fw-bold fs-2" style="color: #986923;">We Rise by lifting
+							<br />Others</p> -->
 							<!--end::Description-->
 						</div>
 						<!--end::Content-->
 						<!--begin::Illustration-->
-						<div class="d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-350px" style="background-image: url(../assets/media/illustrations/sketchy-1/13.png"></div>
+						<div class="d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-350px" style="background-image: url(<?php echo $theme_path; ?>/assets/media/illustrations/sketchy-1/13.png"></div>
 						<!--end::Illustration-->
 					</div>
 					<!--end::Wrapper-->
