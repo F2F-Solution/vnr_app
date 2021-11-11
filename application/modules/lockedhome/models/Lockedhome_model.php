2dc09fd0abe0acc14cde31503d100b8cadf06513
assinbui <assinbui2k21@gmail.com>
2021-11-11 19:50:34 +0530

    Locked home module

---
+++ application/modules/lockedhome/models/Lockedhome_model.php
@@ -0,0 +1,71 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Lockedhome_model extends CI_model{
+    public function __construct(){
+         $table = 'vnr_locked_home';
+         $column_order = array(null, 'S.No',' Customer Name','Phone number','From date','To date','Address','pincode','Identification Proof','Attachment','Actions'); //set column field database for datatable orderable
+         $column_search = array('Customer Name'); //set column field database for datatable searchable 
+         $order = array('id' => 'desc'); // default descending order
+         $this->load->database();   
+    }
+    //list data
+    private function list_data() {        
+        $this->db->from($this->table);
+        $i = 0; 
+        foreach ($this->column_search as $item) 
+        {
+            if($_POST['search']['value']) 
+            {               
+                if($i===0) // first loop
+                {
+                    $this->db->group_start(); 
+                    $this->db->like($item, $_POST['search']['value']);
+                } else {
+                    $this->db->or_like($item, $_POST['search']['value']);
+                }
+                if(count($this->column_search) - 1 == $i) //last loop
+                    $this->db->group_end(); 
+            }
+            $i++;
+        }       
+        if(isset($_POST['order'])) { 
+            $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+        } else if(isset($this->order)) {
+            $order = $this->order;
+            $this->db->order_by(key($order), $order[key($order)]);
+        }
+    }
+    public function listtable_data() {
+        $this->list_data();
+        if($_POST['length'] != -1)
+        $this->db->limit($_POST['length'], $_POST['start']);
+        $this->db->select('*');
+        $this->db->from('vnr_locked_home');
+        $query = $this->db->get();
+        return $query->result();
+    }
+    function count_filtered_gen_posts() {
+        $this->list_data();
+        $query = $this->db->get('vnr_locked_home');
+        return $query->num_rows();
+    }
+     // edit data
+     public function find_data($iLockedHomeId){
+        $this->db->where('iLockedHomeId', $iLockedHomeId);
+        $query = $this->db->get('vnr_locked_home');
+        return $query->row_array();
+    }
+    //update data
+    public function update_data($data,$iLockedHomeId){
+        $this->db->where('iLockedHomeId', $iLockedHomeId);
+        $this->db->update('vnr_locked_home',$data);
+        // print_r($this->db->last_query());exit;
+    }
+     //Delete data  
+     public function delete_data($iLockedHomeId){
+        $this->db->where('iLockedHomeId', $iLockedHomeId);
+        $this-> db->delete('vnr_locked_home');
+    }
+}
